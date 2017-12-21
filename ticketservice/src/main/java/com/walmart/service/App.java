49fa7006baf75a4586806e47f2d100f734079dfb
sadapa <sadapa@cghtech.com>
2017-12-21 00:11:32 -0500

    first commit

---
+++ ticketservice/src/main/java/com/walmart/service/App.java
@@ -0,0 +1,13 @@
+package com.walmart.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
